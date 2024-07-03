16ae8321462eecbff0df8587bb7788177db94723
Rodrich94 <rodrigo.chavez@est.fi.uncoma.edu.ar>
2024-07-03 10:12:22 -0300

    https para consumo de la API

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://tortuga94.pythonanywhere.com',  // Asegúrate de usar el puerto correcto donde tu backend Flask está escuchando
+  baseURL: 'https://tortuga94.pythonanywhere.com',  // Asegúrate de usar el puerto correcto donde tu backend Flask está escuchando
   headers: {
     'Content-Type': 'application/json',
   },
