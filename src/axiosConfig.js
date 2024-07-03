7d676beb21916f7a900fb4a6c98834397b0d9441
Rodrich94 <rodrigo.chavez@est.fi.uncoma.edu.ar>
2024-07-03 10:04:42 -0300

    cambio endpoint API

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',  // Asegúrate de usar el puerto correcto donde tu backend Flask está escuchando
+  baseURL: 'http://tortuga94.pythonanywhere.com',  // Asegúrate de usar el puerto correcto donde tu backend Flask está escuchando
   headers: {
     'Content-Type': 'application/json',
   },
